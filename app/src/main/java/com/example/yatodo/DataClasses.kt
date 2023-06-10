4b135783996638638a0d98c8cbe98f8dd3bab4e0
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-06-10 13:02:45 +0300

    TodoItem and TodoItemsRepository added, generator is not finished yet

---
+++ app/src/main/java/com/example/yatodo/DataClasses.kt
@@ -0,0 +1,46 @@
+package com.example.yatodo
+
+import java.time.LocalDate
+import java.util.Date
+import java.util.function.Predicate
+
+enum class Importance {
+    LOW, MEDIUM, HIGH
+}
+
+data class TodoItem (
+    var taskId: String,
+    var taskText: String,
+    var importance: Importance,
+    var withDeadline: bool, // TODO
+    var deadlineDate: LocalDate,
+    var isCompleted: bool,
+    var creationDate: LocalDate,
+    var changeDate: LocalDate, // TODO
+)
+
+class TodoItemsRepository {
+    var todoItemsList : MutableList<TodoItem> = mutableListOf()
+
+    fun addItem(newItem: TodoItem) {
+        this.todoItemsList.add(newItem)
+    }
+    fun removeItemById(id: String) {
+        this.todoItemsList.removeIf(Predicate { item : TodoItem -> item.taskId == id})
+    }
+    fun generate() {
+        val i1:TodoItem = TodoItem(taskId = "aaa",
+            taskText = "Please buy milk I beg you", isCompleted = false,
+            withDeadline = false, creationDate = LocalDate.now(),
+            changeDate = LocalDate.now(), deadlineDate = LocalDate.now(),
+            importance = Importance.HIGH)
+        val i2:TodoItem
+        val i3:TodoItem
+        val i4:TodoItem
+        val i5:TodoItem // TODO
+        val i6:TodoItem
+        val i7:TodoItem
+        this.addItem(i1)
+
+    }
+}
