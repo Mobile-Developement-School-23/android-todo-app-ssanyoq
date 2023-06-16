df4938b18289d436c35a77108607f650c9a705fb
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-06-16 12:38:20 +0300

    Hiding toolbar and scrollable recycler

--- app/src/main/java/com/example/yatodo/MainActivity.kt
+++ app/src/main/java/com/example/yatodo/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.yatodo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.yatodo.recycler.TodoItemAdapter
+import com.google.android.material.appbar.AppBarLayout
+import com.example.yatodo.TaskMenuActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.imageview.ShapeableImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +26,21 @@ class MainActivity : AppCompatActivity() {
         todoItemsRecyclerView.adapter = todoItemAdapter
         todoItemsRecyclerView.layoutManager = layoutManager
         todoItemAdapter.todoItems = todoItemsRepository.getTodoItems() // (this)?
+
+        val appBarLayout = findViewById<AppBarLayout>(R.id.app_bar_layout)
+        val motionLayout = findViewById<MotionLayout>(R.id.toolbar)
+        appBarLayout.addOnOffsetChangedListener { _, verticalOffset ->
+            val seekPosition = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
+            motionLayout.progress = seekPosition
+        }
+
+        val btn = findViewById<ShapeableImageView>(R.id.visibilityIcon)
+        btn.setOnClickListener {
+            openTaskMenuActivity()
+        }
+    }
+    fun openTaskMenuActivity() {
+        val intent = Intent(this, TaskMenuActivity::class.java)
+        startActivity(intent)
     }
 }
