eef62cbdc2aaf2e97e9fb4761b641ec310030f6c
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-06-28 23:07:20 +0300

    TodoItemsRepository with blackjack and coroutines, update() yet to be coded

--- app/src/main/java/com/example/yatodo/data/TodoItemsRepository.kt
+++ app/src/main/java/com/example/yatodo/data/TodoItemsRepository.kt
@@ -1,43 +1,71 @@
-package com.example.yatodo
+package com.example.yatodo.data
 
 import android.icu.util.Calendar
-import com.example.yatodo.data.Importance
-import com.example.yatodo.data.TodoItem
-import java.time.LocalDate
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 
-class TodoItemsRepository {
-    companion object {
-        var todoItemsList = mutableListOf<TodoItem>()
-        var indexToChange = -1
+class TodoItemsRepository() // TODO
+{
+    private var _todoItemsList = MutableLiveData<List<TodoItem>>(emptyList())
+
+    val todoItemsList: LiveData<List<TodoItem>> = _todoItemsList
+
+
+    fun getItemById(id: String?): TodoItem? {
+        if (id == null) {
+            return null
+        }
+        for (i in 0 until (todoItemsList.value?.size!!)) {
+            if (todoItemsList.value?.get(i)?.taskId == id) {
+                return todoItemsList.value?.get(i)
+            }
+        }
+        return null
     }
 
-    private operator fun set(i: Int, value: TodoItem) {
-        todoItemsList[i] = value
+    /**
+     * Changes value of `isChecked` of `TodoItem` with id [taskId] to [isChecked].
+     * Doesn't do anything if there is no item with such id
+     */
+    suspend fun checkItemById(taskId: String, isChecked: Boolean) {
+        val newItems = withContext(Dispatchers.Default) {
+            todoItemsList.value.orEmpty()
+                .map { item -> if (item.taskId == taskId) item.copy(isCompleted = isChecked) else item }
+        }
+        _todoItemsList.value = newItems
     }
 
-    operator fun get(i: Int): TodoItem {
-        return todoItemsList[i]
+    /**
+     * Deletes `TodoItem` with given [taskId]. Nothing will change if there is no item with such id
+     */
+    suspend fun deleteItemById(taskId: String) {
+        val newItems = withContext(Dispatchers.Default) {
+            todoItemsList.value.orEmpty().filter { it -> it.taskId != taskId }
+        }
+        _todoItemsList.value = newItems
     }
 
-    fun set(list: MutableList<TodoItem>) {
-        todoItemsList = list
+    suspend fun countDone(): Int {
+        val doneAmount = withContext(Dispatchers.Default) {
+            todoItemsList.value?.count { item -> item.isCompleted }!!
+        }
+        return doneAmount
     }
 
-    fun getTodoItems(): MutableList<TodoItem> {
-        return todoItemsList
+    @MainThread
+    fun updateTodoItems() {
+        // TODO
+        Log.e("TodoItemsRepository", "Please make update function")
     }
 
-    fun addItem(newItem: TodoItem) {
-        todoItemsList.add(newItem)
-    }
-
-    fun removeItemById(id: String) {
-        todoItemsList.removeIf { item: TodoItem -> item.taskId == id }
-    }
-
-    fun countDone(): Int {
-        return todoItemsList.count { item -> item.isCompleted }
+    fun addItem(todoItem: TodoItem) {
+        // Temporary function just for generate()
+        _todoItemsList.value = _todoItemsList.value?.plus(todoItem)
     }
 
     fun generate() {
@@ -50,7 +78,7 @@ class TodoItemsRepository {
         )
         val i2: TodoItem = TodoItem(
             taskId = "bbb",
-            text = "Some text but with no deadline date", isCompleted = false,
+            text = "Some text but with no deadline date", isCompleted = true,
             createdAt = calendar.time, deadline = null,
             importance = Importance.HIGH, modifiedAt = null
         )
