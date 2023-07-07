ce22edec684dde1bcf82586b42a5c1948638e0c8
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-07-07 16:58:09 +0300

    Pretty TaskFragment

--- app/src/main/java/com/example/yatodo/task_screen/TaskFragment.kt
+++ app/src/main/java/com/example/yatodo/task_screen/TaskFragment.kt
@@ -1,9 +1,8 @@
-package com.example.yatodo
+package com.example.yatodo.task_screen
 
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
-import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.PopupMenu
@@ -15,6 +14,8 @@ import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
+import com.example.yatodo.R
+import com.example.yatodo.components.TaskFragmentComponent
 import com.example.yatodo.data.Importance
 import com.example.yatodo.data.InteractionType
 import com.example.yatodo.data.TodoItem
@@ -27,109 +28,81 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-class TaskFragment() : Fragment(R.layout.task_fragment) {
+class TaskFragment : Fragment(R.layout.task_fragment) {
+
+    private val taskFragmentComponent = TaskFragmentComponent(arguments)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val closeButton = view.findViewById<FloatingActionButton>(R.id.close_button)
         val importancePopup = view.findViewById<TextView>(R.id.popup_button)
         val datePickerIndicator = view.findViewById<TextView>(R.id.datepicker_indicator)
         val datePickerButton = view.findViewById<SwitchCompat>(R.id.toggle_date_button)
-        val taskDescription = view.findViewById<EditText>(R.id.task_description)
+
         val saveButton = view.findViewById<MaterialButton>(R.id.save_button)
         val deleteButton = view.findViewById<ConstraintLayout>(R.id.delete_layout)
 
-        val todoItem: TodoItem? = arguments?.getParcelable("todo_item")
-
-        var pickedDate: Date? = null
-        var taskText: String? = null
-        var importance: Importance = Importance.COMMON
-
 
         closeButton.setOnClickListener {
-            setFragmentResult(
-                "task_fragment",
-                bundleOf("todo_item" to null, "interaction_type" to InteractionType.Nothing)
-            )
-            parentFragmentManager.popBackStack()
+            setResultAndPop(null, InteractionType.Nothing)
         }
         deleteButton.setOnClickListener {
-            setFragmentResult(
-                "task_fragment",
-                bundleOf("todo_item" to todoItem, "interaction_type" to InteractionType.DeleteItem)
-            )
-            parentFragmentManager.popBackStack()
+            setResultAndPop(taskFragmentComponent.todoItem, InteractionType.DeleteItem)
         }
         saveButton.setOnClickListener {
-            taskText = taskDescription.text.toString()
-            val newItem = TodoItem(
-                "To be changed", // definitely will be changed
-                taskText!!,
-                importance,
-                pickedDate,
-                false,
-                Date(System.currentTimeMillis()),
-                null
-            )
-            if (todoItem == null) {
-                setFragmentResult(
-                    "task_fragment",
-                    bundleOf("todo_item" to newItem, "interaction_type" to InteractionType.AddItem)
-                )
-            } else {
-                newItem.taskId = todoItem.taskId
-                setFragmentResult(
-                    "task_fragment",
-                    bundleOf(
-                        "todo_item" to newItem,
-                        "interaction_type" to InteractionType.ChangeItem
-                    )
-                )
-            }
-            parentFragmentManager.popBackStack()
+            onSaveButtonPress()
         }
         // Popup menu handling
         importancePopup.setOnClickListener {
-            val popupMenu = PopupMenu(this.requireContext(), importancePopup)
-            popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
-            popupMenu.setForceShowIcon(true)
-
-            // High importance element drawing
-            val importanceHighItem = popupMenu.menu.getItem(2)
-            importanceHighItem.icon = AppCompatResources.getDrawable(
-                this.requireContext(),
-                R.drawable.double_exclamation
-            )
-            val s = SpannableString(importanceHighItem.title)
-            s.setSpan(
-                ForegroundColorSpan(ContextCompat.getColor(this.requireContext(), R.color.red)),
-                0, s.length, 0
-            )
-            importanceHighItem.title = s
-
-            // Low importance element drawing
-            val importanceLowItem = popupMenu.menu.getItem(1)
-            importanceLowItem.icon = AppCompatResources.getDrawable(
-                this.requireContext(),
-                R.drawable.arrow_down
-            )
-            popupMenu.setOnMenuItemClickListener { item ->
-                importancePopup.text = item.title
-                when (item.itemId) {
-                    R.id.importance_none ->
-                        importance = Importance.COMMON
-
-                    R.id.importance_low ->
-                        importance = Importance.LOW
-
-                    R.id.importance_high ->
-                        importance = Importance.HIGH
-                }
-                true
-            }
-            popupMenu.show()
+            onImportancePopupPress()
         }
 
         // Date picker construction
+
+
+        // Date picker call
+
+        datePickerButton.setOnClickListener { button ->
+            onDatePickerButtonPress(button)
+        }
+
+        // setting passed todoItem everywhere
+        val taskDescription = view.findViewById<EditText>(R.id.task_description)
+        if (taskFragmentComponent.todoItem != null) {
+            taskDescription.setText(
+                taskFragmentComponent.todoItem.text,
+                TextView.BufferType.EDITABLE
+            )
+            importancePopup.text = getString(taskFragmentComponent.todoItem.importance.stringId())
+            if (taskFragmentComponent.todoItem.deadline != null) {
+                datePickerButton.isChecked = true
+                datePickerIndicator.text = setDate(taskFragmentComponent.todoItem.deadline!!)
+            }
+
+            taskFragmentComponent.importance = taskFragmentComponent.todoItem.importance
+            taskFragmentComponent.pickedDate = taskFragmentComponent.todoItem.deadline
+
+        }
+    }
+
+    private fun setDate(date: Date): String {
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.US)
+        return sdf.format(date)
+    }
+
+    private fun onSaveButtonPress() {
+        val taskDescription = view?.findViewById<EditText>(R.id.task_description)
+        taskFragmentComponent.taskText = taskDescription?.text.toString()
+        val newItem = taskFragmentComponent.composeTodoItem()
+        var interaction = InteractionType.AddItem
+        if (taskFragmentComponent.todoItem != null) {
+            newItem?.taskId = taskFragmentComponent.todoItem.taskId
+            interaction = InteractionType.ChangeItem
+        }
+        setResultAndPop(newItem, interaction)
+    }
+
+    private fun onDatePickerButtonPress(button: View?) {
+        button as SwitchCompat
         val constraintsBuilder = CalendarConstraints.Builder()
             .setStart(MaterialDatePicker.todayInUtcMilliseconds())
         val datePicker = MaterialDatePicker.Builder.datePicker()
@@ -141,50 +114,74 @@ class TaskFragment() : Fragment(R.layout.task_fragment) {
                 constraintsBuilder.build()
             )
             .build()
-
-        // Date picker call
-
-        datePickerButton.setOnClickListener { it ->
-            it as SwitchCompat
-            if (it.isChecked) {
-                datePicker.show(this.parentFragmentManager, "tag")
-                datePicker.addOnPositiveButtonClickListener { it2 ->
-                    pickedDate = Date(it2)
-                    datePickerIndicator.text = setDate(pickedDate!!)
-                }
-                datePicker.addOnDismissListener {
-                    if (pickedDate == null) {
-                        datePickerButton.isChecked = false
-                    }
-                }
-            } else {
-                pickedDate = null
-                datePickerIndicator.text = ""
+        val datePickerIndicator = view?.findViewById<TextView>(R.id.datepicker_indicator)
+        val datePickerButton = view?.findViewById<SwitchCompat>(R.id.toggle_date_button)
+        if (button.isChecked) {
+            datePicker.show(this.parentFragmentManager, "tag")
+            datePicker.addOnPositiveButtonClickListener { it2 ->
+                taskFragmentComponent.pickedDate = Date(it2)
+                datePickerIndicator?.text = setDate(taskFragmentComponent.pickedDate!!)
             }
-        }
-
-        // setting passed todoItem everywhere
-
-        if (todoItem != null) {
-            taskDescription.setText(todoItem.text, TextView.BufferType.EDITABLE)
-            importancePopup.text = getString(todoItem.importance.stringId())
-            if (todoItem.deadline != null) {
-                datePickerButton.isChecked = true
-                datePickerIndicator.text = setDate(todoItem.deadline!!)
+            datePicker.addOnDismissListener {
+                if (taskFragmentComponent.pickedDate == null) {
+                    datePickerButton?.isChecked = false
+                }
             }
-
-            importance = todoItem.importance
-            pickedDate = todoItem.deadline
-
+        } else {
+            taskFragmentComponent.pickedDate = null
+            datePickerIndicator?.text = ""
         }
     }
 
-    fun setDate(date: Any): String {
-        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.US)
-        return when (date) {
-            is Long -> sdf.format(date)
-            is Date -> sdf.format(date)
-            else -> throw IllegalArgumentException("Unsupported type")
+    private fun onImportancePopupPress() {
+        val importancePopup = view?.findViewById<TextView>(R.id.popup_button)
+        val popupMenu = PopupMenu(this.requireContext(), importancePopup)
+        popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
+        popupMenu.setForceShowIcon(true)
+
+        // High importance element drawing
+        val importanceHighItem = popupMenu.menu.getItem(2)
+        importanceHighItem.icon = AppCompatResources.getDrawable(
+            this.requireContext(),
+            R.drawable.double_exclamation
+        )
+        val s = SpannableString(importanceHighItem.title)
+        s.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(this.requireContext(), R.color.red)),
+            0, s.length, 0
+        )
+        importanceHighItem.title = s
+
+        // Low importance element drawing
+        val importanceLowItem = popupMenu.menu.getItem(1)
+        importanceLowItem.icon = AppCompatResources.getDrawable(
+            this.requireContext(),
+            R.drawable.arrow_down
+        )
+        popupMenu.setOnMenuItemClickListener { item ->
+            importancePopup?.text = item.title
+            when (item.itemId) {
+                R.id.importance_none ->
+                    taskFragmentComponent.importance = Importance.COMMON
+
+                R.id.importance_low ->
+                    taskFragmentComponent.importance = Importance.LOW
+
+                R.id.importance_high ->
+                    taskFragmentComponent.importance = Importance.HIGH
+            }
+            true
         }
+        popupMenu.show()
+    }
+
+    private fun setResultAndPop(todoItem: TodoItem?, interactionType: InteractionType) {
+        setFragmentResult(
+            "task_fragment", bundleOf(
+                "todo_item" to todoItem,
+                "interaction_type" to interactionType
+            )
+        )
+        parentFragmentManager.popBackStack()
     }
 }
