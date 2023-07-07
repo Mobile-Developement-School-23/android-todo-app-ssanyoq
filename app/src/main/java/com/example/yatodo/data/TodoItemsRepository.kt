e9aa3f88dfc14a3addf6d1672964c7ff949df20c
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-07-07 09:13:07 +0300

    New change, add and delete functions

--- app/src/main/java/com/example/yatodo/data/TodoItemsRepository.kt
+++ app/src/main/java/com/example/yatodo/data/TodoItemsRepository.kt
@@ -1,7 +1,6 @@
 package com.example.yatodo.data
 
 import android.icu.util.Calendar
-import android.util.Log
 import androidx.annotation.MainThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -40,6 +39,16 @@ class TodoItemsRepository(private val dataSource: NetworkDataResource)
         _todoItemsList.value = newItems
     }
 
+    suspend fun changeItemById(taskId: String, newItem: TodoItem) {
+        val newItems = withContext(Dispatchers.Default) {
+            newItem.taskId = taskId // just in case
+            todoItemsList.value.orEmpty()
+                .map {item -> if (item.taskId == taskId) newItem else item}
+        }
+        _todoItemsList.value = newItems
+    }
+
+
     /**
      * Deletes `TodoItem` with given [taskId]. Nothing will change if there is no item with such id
      */
@@ -50,6 +59,20 @@ class TodoItemsRepository(private val dataSource: NetworkDataResource)
         _todoItemsList.value = newItems
     }
 
+    /**
+    * Adds [todoItem] and generates `taskId` for it (equals `size.toString()`)
+     */
+    suspend fun addItem(todoItem: TodoItem?) {
+        if (todoItem == null) {
+            return
+        }
+        val newItems = withContext(Dispatchers.Default) {
+            todoItem.taskId = todoItemsList.value.orEmpty().size.toString()
+            todoItemsList.value.orEmpty().plus(todoItem)
+        }
+        _todoItemsList.value = newItems
+    }
+    @MainThread
     suspend fun countDone(): Int {
         val doneAmount = withContext(Dispatchers.Default) {
             todoItemsList.value?.count { item -> item.isCompleted }!!
@@ -63,32 +86,29 @@ class TodoItemsRepository(private val dataSource: NetworkDataResource)
         _todoItemsList.value = loadedData
     }
 
-    fun addItem(todoItem: TodoItem) {
-        // Temporary function just for generate()
-        _todoItemsList.value = _todoItemsList.value?.plus(todoItem)
-    }
 
-    fun generate() {
+
+    suspend fun generate() {
         val calendar = Calendar.getInstance()
-        val i1: TodoItem = TodoItem(
+        val i1 = TodoItem(
             taskId = "aaa",
             text = "Some text but completed", isCompleted = true,
             createdAt = calendar.time, deadline = calendar.time,
             importance = Importance.LOW, modifiedAt = null
         )
-        val i2: TodoItem = TodoItem(
+        val i2 = TodoItem(
             taskId = "bbb",
             text = "Some text but with no deadline date", isCompleted = true,
             createdAt = calendar.time, deadline = null,
             importance = Importance.HIGH, modifiedAt = null
         )
-        val i3: TodoItem = TodoItem(
+        val i3 = TodoItem(
             taskId = "c",
             text = "Some text but with \nthese \nthings", isCompleted = false,
             createdAt = calendar.time, deadline = calendar.time,
             importance = Importance.COMMON, modifiedAt = null
         )
-        val i4: TodoItem = TodoItem(
+        val i4 = TodoItem(
             taskId = "ddd",
             text = "Some really really really really really really really really really really really really really really really really really really long text",
             isCompleted = false,
@@ -97,19 +117,19 @@ class TodoItemsRepository(private val dataSource: NetworkDataResource)
             importance = Importance.HIGH,
             modifiedAt = null
         )
-        val i5: TodoItem = TodoItem(
+        val i5 = TodoItem(
             taskId = "eee",
             text = "Please", isCompleted = false,
             createdAt = calendar.time, deadline = calendar.time,
             importance = Importance.LOW, modifiedAt = null
         )
-        val i6: TodoItem = TodoItem(
+        val i6 = TodoItem(
             taskId = "fff",
             text = "It's 3AM", isCompleted = false,
             createdAt = calendar.time, deadline = calendar.time,
             importance = Importance.COMMON, modifiedAt = null
         )
-        val i7: TodoItem = TodoItem(
+        val i7 = TodoItem(
             taskId = "ggg",
             text = "Help me", isCompleted = false,
             createdAt = calendar.time, deadline = calendar.time,
