abea92bca1db5fcbb9f48379df5a1e5f738b4cbf
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-07-07 09:14:09 +0300

    onItemOpened implementation, countDone, and, change and delete item functions

--- app/src/main/java/com/example/yatodo/viewmodel/TodoItemsViewModel.kt
+++ app/src/main/java/com/example/yatodo/viewmodel/TodoItemsViewModel.kt
@@ -1,12 +1,17 @@
 package com.example.yatodo.viewmodel
 
+import android.view.View
+import androidx.core.os.bundleOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.Navigation
+import com.example.yatodo.R
+import com.example.yatodo.data.InteractionType
 import com.example.yatodo.data.TodoItem
 import com.example.yatodo.data.TodoItemsRepository
 import kotlinx.coroutines.launch
 
-class TodoItemsViewModel (private val todoItemsRepository: TodoItemsRepository): ViewModel() {
+class TodoItemsViewModel(private val todoItemsRepository: TodoItemsRepository) : ViewModel() {
     val todoItems = todoItemsRepository.todoItemsList
 
     init {
@@ -18,12 +23,39 @@ class TodoItemsViewModel (private val todoItemsRepository: TodoItemsRepository):
             todoItemsRepository.updateTodoItems()
         }
     }
-    fun onItemOpened(todoItem: TodoItem) {
-        // TODO
+
+    fun onItemOpened(todoItem: TodoItem, view: View) {
+        Navigation.findNavController(view).navigate(
+            R.id.action_main_to_task_fragment, bundleOf(
+                "todo_item" to todoItem,
+                "interaction_type" to InteractionType.ChangeItem
+            )
+        )
     }
+
     fun onItemChecked(todoItem: TodoItem, isChecked: Boolean) {
         viewModelScope.launch {
             todoItemsRepository.checkItemById(todoItem.taskId, isChecked)
         }
+    }
+
+    suspend fun countDone(): Int {
+        if (todoItemsRepository.todoItemsList.value?.size == 0) {
+            return 0
+        } else {
+            return todoItemsRepository.countDone()
+        }
+    }
+
+    suspend fun addItem(todoItem: TodoItem?) {
+        todoItemsRepository.addItem(todoItem)
+    }
+
+    suspend fun changeItem(todoItem: TodoItem) {
+        todoItemsRepository.changeItemById(todoItem.taskId, todoItem)
+    }
+
+    suspend fun deleteItem(todoItem: TodoItem) {
+        todoItemsRepository.deleteItemById(todoItem.taskId)
     }
 }
