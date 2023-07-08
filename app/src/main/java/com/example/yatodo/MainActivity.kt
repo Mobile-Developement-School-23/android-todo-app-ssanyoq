aa6b9cf272483c060aba1a70b448a8d552b10bb3
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-07-08 08:08:27 +0300

    Unnecessary imports deleted

--- app/src/main/java/com/example/yatodo/MainActivity.kt
+++ app/src/main/java/com/example/yatodo/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.example.yatodo
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.content.res.AppCompatResources
-import androidx.constraintlayout.motion.widget.MotionLayout
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.yatodo.main_screen.FragmentMain
-import com.example.yatodo.recycler.TodoItemAdapter
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.imageview.ShapeableImageView
 
 class MainActivity : AppCompatActivity() {
+    /**
+     *
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
