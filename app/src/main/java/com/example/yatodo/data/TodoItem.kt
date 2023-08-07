cfdc48dd9358c4cd4524c3dd761c692b04f31dc8
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-08-07 17:55:23 +0300

    Importance.apiString function and secondary constructor for TodoItem

--- app/src/main/java/com/example/yatodo/data/TodoItem.kt
+++ app/src/main/java/com/example/yatodo/data/TodoItem.kt
@@ -2,20 +2,58 @@ package com.example.yatodo.data
 
 import android.os.Parcelable
 import com.example.yatodo.R
+import com.example.yatodo.network.SerializedTodoItem
 import kotlinx.parcelize.Parcelize
+import java.lang.IllegalArgumentException
 import java.util.Date
 
 enum class Importance {
     LOW, COMMON, HIGH
 }
+
+/**
+ * Converts [Importance] to a resource that is used in UI
+ */
 fun Importance.stringId() =
     when (this) {
         Importance.COMMON -> R.string.importance_none
         Importance.HIGH -> R.string.importance_high
         Importance.LOW -> R.string.importance_low
     }
+
+/**
+ * Converts [Importance] to a [String] that is recognisable by an API
+ */
+fun Importance.apiString() =
+    when (this) {
+        Importance.COMMON -> "basic"
+        Importance.LOW -> "low"
+        Importance.HIGH -> "important"
+    }
+
+/**
+ * Converts [String] importance to [Importance]
+ *
+ * Made to work with strings recieved via API:
+ *
+ * `"low"` -> [Importance.LOW]
+ *
+ * `"basic"` -> [Importance.COMMON]
+ *
+ * `"important"` -> [Importance.HIGH]
+ *
+ * @exception IllegalArgumentException if string differs from strings above
+ */
+fun enumFromString(string: String): Importance =
+    when (string) {
+        "low" -> Importance.LOW
+        "basic" -> Importance.COMMON
+        "important" -> Importance.HIGH
+        else -> throw IllegalArgumentException("Unable to convert $string into Importance value")
+    }
+
 @Parcelize
-data class TodoItem(
+data class TodoItem constructor(
     var taskId: String,
     var text: String,
     var importance: Importance,
@@ -23,4 +61,15 @@ data class TodoItem(
     var isCompleted: Boolean,
     var createdAt: Date,
     var modifiedAt: Date?,
-): Parcelable
+) : Parcelable {
+
+    // For better SerializedTodoItem -> TodoItem conversion
+    constructor(serializedTodoItem: SerializedTodoItem) : this(
+        serializedTodoItem.id,
+        serializedTodoItem.text,
+        enumFromString(serializedTodoItem.importance),
+        serializedTodoItem.deadline?.let { Date(it) },
+        serializedTodoItem.done,
+        Date(serializedTodoItem.createdAt!!),
+        serializedTodoItem.changedAt?.let { Date(it) })
+}
