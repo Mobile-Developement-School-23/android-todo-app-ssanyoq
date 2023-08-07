c4af9c61d25596ae35c9a60ab58df99bccec6307
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-08-07 18:22:21 +0300

    Unnecessary strings deleted

--- app/src/main/java/com/example/yatodo/network/ApiConstants.kt
+++ app/src/main/java/com/example/yatodo/network/ApiConstants.kt
@@ -2,15 +2,9 @@ package com.example.yatodo.network
 
 object ApiConstants {
     const val BASE_URL = "https://beta.mrdekk.ru/todobackend/"
-
-    const val KEY_INSERT = "insert_items"
-    const val KEY_UPDATE = "update_items"
-    const val KEY_DELETE = "delete_items"
-
     const val LAST_KNOWN_REVISION = "X-Last-Known-Revision"
 
     const val GENERATE_FAILS = "X-Generate-Fails"
-
 
     const val API_TOKEN = "discussionis" // top secret
 }
