1693c0ecbf6bb85f4ab3fef37a5f953ce6a538e8
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-08-08 13:03:07 +0300

    Dummy data source is now removed, network features are implemented

--- app/src/main/java/com/example/yatodo/components/ApplicationComponent.kt
+++ app/src/main/java/com/example/yatodo/components/ApplicationComponent.kt
@@ -1,12 +1,10 @@
 package com.example.yatodo.components
 
-import com.example.yatodo.data.NetworkDataResource
 import com.example.yatodo.data.TodoItemsRepository
 import com.example.yatodo.viewmodel.ViewModelFactory
 
 class ApplicationComponent {
-    private val dataSource = NetworkDataResource()
-    private val todoItemsRepository = TodoItemsRepository(dataSource)
+    private val todoItemsRepository = TodoItemsRepository()
 
     /**
      * Public getter allows all other classes to use same instance from [ApplicationComponent].
--- app/src/main/java/com/example/yatodo/data/TodoItemsRepository.kt
+++ app/src/main/java/com/example/yatodo/data/TodoItemsRepository.kt
@@ -1,9 +1,9 @@
 package com.example.yatodo.data
 
-import android.util.Log
 import androidx.annotation.MainThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.yatodo.network.TodoApiImpl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -11,13 +11,13 @@ import javax.inject.Singleton
 
 
 /**
- * Repository of `TodoItem`s. When initialized tries to get data from [dataSource].
+ * Repository of [TodoItem]s. When initialized tries to get data from [TodoApiImpl].
  * TodoItems are stored in `LiveData`
  *
  * Most operations are done in background threads
  */
 @Singleton
-class TodoItemsRepository @Inject constructor(private val dataSource: NetworkDataResource) {
+class TodoItemsRepository @Inject constructor() {
     private var _todoItemsList = MutableLiveData<List<TodoItem>>(emptyList())
 
     val todoItemsList: LiveData<List<TodoItem>> = _todoItemsList
@@ -32,6 +32,11 @@ class TodoItemsRepository @Inject constructor(private val dataSource: NetworkDat
                 .map { item -> if (item.taskId == taskId) item.copy(isCompleted = isChecked) else item }
         }
         _todoItemsList.value = newItems
+
+        withContext(Dispatchers.IO) {
+            TodoApiImpl.checkItemById(taskId)
+        }
+
     }
 
     /**
@@ -45,6 +50,11 @@ class TodoItemsRepository @Inject constructor(private val dataSource: NetworkDat
                 .map { item -> if (item.taskId == taskId) newItem else item }
         }
         _todoItemsList.value = newItems
+
+        withContext(Dispatchers.IO) {
+            newItem.taskId = taskId // yet again
+            TodoApiImpl.updateItem(newItem)
+        }
     }
 
 
@@ -58,21 +68,26 @@ class TodoItemsRepository @Inject constructor(private val dataSource: NetworkDat
         }
         _todoItemsList.value = newItems
 
+        withContext(Dispatchers.IO) {
+            TodoApiImpl.deleteItem(taskId)
+        }
+
     }
 
     /**
      * Adds [todoItem] and generates `taskId` for it (equals `size.toString()`)
      */
-    suspend fun addItem(todoItem: TodoItem?) {
-        if (todoItem == null) {
-            return
-        }
+    suspend fun addItem(todoItem: TodoItem) {
         val newItems = withContext(Dispatchers.Default) {
             todoItem.taskId = todoItemsList.value.orEmpty().size.toString()
             todoItemsList.value.orEmpty().plus(todoItem)
         }
+
         _todoItemsList.value = newItems
-        Log.i("Items", "Item added. New length is " + todoItemsList.value?.size.toString())
+
+        withContext(Dispatchers.IO) {
+            TodoApiImpl.addItem(todoItem)
+        }
     }
 
     /**
@@ -88,7 +103,9 @@ class TodoItemsRepository @Inject constructor(private val dataSource: NetworkDat
 
     @MainThread
     suspend fun updateTodoItems() {
-        val loadedData = withContext(Dispatchers.IO) { dataSource.loadTasks() }
+        val loadedData = withContext(Dispatchers.IO) {
+            TodoApiImpl.getList()
+        }
         _todoItemsList.value = loadedData
     }
 }
