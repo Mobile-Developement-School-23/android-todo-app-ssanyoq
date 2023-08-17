5447a0d48dffb4b9179a834ef99784e2f4eed076
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-08-17 10:39:14 +0600

    DB initialization and passing to repository

--- app/src/main/java/com/example/yatodo/components/ApplicationComponent.kt
+++ app/src/main/java/com/example/yatodo/components/ApplicationComponent.kt
@@ -1,10 +1,13 @@
 package com.example.yatodo.components
 
+import android.app.Application
 import com.example.yatodo.data.TodoItemsRepository
+import com.example.yatodo.database.TodoDatabase
 import com.example.yatodo.viewmodel.ViewModelFactory
 
-class ApplicationComponent {
-    private val todoItemsRepository = TodoItemsRepository()
+class ApplicationComponent(application: Application) {
+    val database = TodoDatabase.getDatabase(application.applicationContext).todoDao()
+    private val todoItemsRepository = TodoItemsRepository(database)
 
     /**
      * Public getter allows all other classes to use same instance from [ApplicationComponent].
