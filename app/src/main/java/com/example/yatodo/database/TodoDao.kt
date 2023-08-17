1f46c6ad2734edf250c43ae5df89d810ae2cef62
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-08-17 10:36:48 +0600

    Removing of Flow

--- app/src/main/java/com/example/yatodo/database/TodoDao.kt
+++ app/src/main/java/com/example/yatodo/database/TodoDao.kt
@@ -7,12 +7,11 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.example.yatodo.data.TodoItem
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TodoDao {
     @Query("SELECT * FROM todo_items")
-    fun getItems(): Flow<List<TodoItem>>
+    fun getItems(): List<TodoItem>
 
     @Query("SELECT * FROM todo_items WHERE taskId = :id")
     fun getItem(id: String): TodoItem
